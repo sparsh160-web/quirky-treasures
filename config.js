9cb56efd01a4b063c9f0d61a41dfc8efa7cca95c
sparsh160-web <sparsh160@gmail.com>
2026-01-04 03:28:10 -0800

    Set SHEET_CSV_URL to published Google Sheet CSV

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-/* Configuration file for Quirky Treasures
+/* Configuration file for Quirky Treasures
  *
  * Replace the value of SHEET_CSV_URL with the publicly published CSV
- * URL of your Google Sheet. See README.md for instructions on how to
+ * URL of your Google Sheet. See README.md for instructions on how to
  * publish your sheet to the web and obtain this URL. Leave it as
  * "PASTE_PUBLISHED_CSV_URL_HERE" until you have generated the link.
  */
 
-const SHEET_CSV_URL = "PASTE_PUBLISHED_CSV_URL_HERE";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ33c86ro6tRuyEQiKraaFHUAm1DDhsW1XQvCTsedCwEtNL59C85x2EPEDlq-sRE8G1qAhMVX0P3qaN/pub?output=csv";
